4966fd1ead10ae300102e7a2b107e97b9b132350
kiarash <shahedikiarash@gmail.com>
2024-01-09 18:41:36 +0330

    adding user to admin page

--- custom_loggin/urls.py
+++ custom_loggin/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import login, dashboard, verify, register_view
 
 urlpatterns = [
-    path('register', register_view, name="register"),
+    path('', register_view, name="register"),
     path('dashboard/', dashboard, name='dashboard'),
     path('verify/', verify, name='verify' ), 
  ]
