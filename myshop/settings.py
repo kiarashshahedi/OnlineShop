3e62be7a9a2d1240d9c458a5f2913365ab34ae1b
Kiarash <shahedikiarash@gmail.com>
2024-05-01 20:07:20 +0330

    fix loading font

--- myshop/settings.py
+++ myshop/settings.py
@@ -16,11 +16,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'custom_loggin/static'),
-    os.path.join(BASE_DIR, 'products/static'),
+    # os.path.join(BASE_DIR, 'custom_loggin/static'),
+    # os.path.join(BASE_DIR, 'products/static'),
+    os.path.join(BASE_DIR, 'static')
     ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Dummy value for development
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Dummy value for development
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
